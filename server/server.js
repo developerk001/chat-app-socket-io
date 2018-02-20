f98a16fc01080c51682b4d2f30f3b37aeecac8e9
designerSohail <designersohail786@gmail.com>
2018-02-20 18:51:30 +0545

    Sockets were added two both client and server

--- server/server.js
+++ server/server.js
@@ -1,17 +1,23 @@
 const path = require('path')
 const express = require('express')
+const http = require('http')
+const socketIO = require('socket.io')
 
 const publicPath = path.join(__dirname, '/../public')
 const port = process.env.PORT || 3000
-
 const app = express()
+const server = http.createServer(app)
+const io = socketIO(server)
 
 app.use(express.static(publicPath))
 
-// app.get('/', (req, res) => {
-//   res.render(publicPath)
-// })
+io.on('connection', socket => {
+  console.log('new user connected')
+  socket.on('disconnect', () => {
+    console.log('Client Disconnected')
+  })
+})
 
-app.listen(port, () => {
+server.listen(port, () => {
   console.log('Server is running on port ' + port)
 })
