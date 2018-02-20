15c6cec481c73783f2261e680297b5309049b00d
designerSohail <designersohail786@gmail.com>
2018-02-20 09:46:40 +0545

    Initial files were added

---
+++ server/server.js
@@ -0,0 +1,17 @@
+const path = require('path')
+const express = require('express')
+
+const publicPath = path.join(__dirname, '/../public')
+const port = process.env.PORT || 3000
+
+const app = express()
+
+app.use(express.static(publicPath))
+
+// app.get('/', (req, res) => {
+//   res.render(publicPath)
+// })
+
+app.listen(port, () => {
+  console.log('Server is running on port ' + port)
+})
